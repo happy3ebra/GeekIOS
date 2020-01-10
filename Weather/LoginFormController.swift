64d9fe93fd4a3f9b5e4ee59a72a49fe9fea83ca8
Aleksei Varlamov <castrtroy@gmail.com>
2020-01-10 15:07:53 +0300

    first constraint

---
+++ Weather/LoginFormController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginFormController.swift
+//  Weather
+//
+//  Created by Macbook on 10.12.2019.
+//  Copyright © 2019 CASTRTROY TECHNOLOGY LIMITED. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+/*
+    func createToolbar() {
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(dismissKeyboard))
+        
+        toolbar.setItems([doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+        
+        UITextField.inputAccessoryView = toolbar
+        
+        // Castomization
+        toolbar.tintColor = .white
+        toolbar.barTintColor = .brown
+    }
+    
+*/
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
