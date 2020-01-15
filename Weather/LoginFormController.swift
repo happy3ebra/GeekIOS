5267413b7c3571aea119f486c011a9f0dd7ffcba
Aleksei Varlamov <castrtroy@gmail.com>
2020-01-15 21:56:45 +0300

    Add source cod + vi

--- Weather/LoginFormController.swift
+++ Weather/LoginFormController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class LoginFormController: UIViewController {
+    
+    
+    @IBOutlet weak var loginText: UILabel!
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var passwordText: UILabel!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBOutlet weak var ButtonLogin: UIButton!
+    
+    @IBAction func ButtonAction(_ sender: Any) {
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
