286d740ca8fa26bb686d68005a6a464113f580fb
Aleksei Varlamov <castrtroy@gmail.com>
2020-01-27 13:38:50 +0300

    correct screen layout

---
+++ Weather/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Weather
+//
+//  Created by Macbook on 23.01.2020.
+//  Copyright © 2020 CASTRTROY TECHNOLOGY LIMITED. All rights reserved.
+//
+
+import Foundation
---
+++ Weather/Friends.swift
@@ -0,0 +1,33 @@
+//
+//  Friends.swift
+//  Weather
+//
+//  Created by Macbook on 23.01.2020.
+//  Copyright © 2020 CASTRTROY TECHNOLOGY LIMITED. All rights reserved.
+//
+
+import UIKit
+
+class Friends: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
